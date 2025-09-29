95a85726e2f404ad016326db16e07cceacc970df
qw3rzz <jenda.vasicek8@gmail.com>
2025-09-29 21:31:47 +0200

    Option to choose if u want numbers in password, fixes #2

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import random
 import string
 
-def generate_password(length=8):
-    chars = string.ascii_letters + string.digits
+def generate_password(length=8, use_digits=True):
+    chars = string.ascii_letters
+    if use_digits:
+        chars += string.digits
     return ''.join(random.choice(chars) for i in range(length))
 
 length = int(input("Enter password length: "))
-print("Generated password:", generate_password(length))
+use_digits = input("Use digits? (y/n): ").lower() == "y"
+
+print("Generated password:", generate_password(length, use_digits))
