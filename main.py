0aec4ea6a12a0942c1646f72b0eb350f98004a16
qw3rzz <jenda.vasicek8@gmail.com>
2025-09-29 21:43:03 +0200

    Add option to use special characters, fixes #4

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import random
 import string
 
-def generate_password(length=8, use_digits=True):
+def generate_password(length=8, use_digits=True, use_specials=True):
     chars = string.ascii_letters
     if use_digits:
         chars += string.digits
+    if use_specials:
+        safe_specials = "!@#$%&*-_"
+        chars += safe_specials
     return ''.join(random.choice(chars) for i in range(length))
 
 length = int(input("Enter password length: "))
 use_digits = input("Use digits? (y/n): ").lower() == "y"
+use_specials = input("Use special characters? (y/n): ").lower() == "y"
 
-print("Generated password:", generate_password(length, use_digits))
+print("Generated password:", generate_password(length, use_digits, use_specials))
