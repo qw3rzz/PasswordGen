f120cee213fbc369ef3f7f4baac7e025342035b9
qw3rzz <jenda.vasicek8@gmail.com>
2025-09-29 21:04:27 +0200

    first commit. base version of passwordgen

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def generate_password(length=8):
+    chars = string.ascii_letters + string.digits
+    return ''.join(random.choice(chars) for i in range(length))
+
+print("Generated password:", generate_password())
